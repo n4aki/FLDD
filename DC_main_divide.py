e12e6f52c427984dfce4bd0da6b1d0241deeb5b3
aki <144330433+n4aki@users.noreply.github.com>
2024-09-17 21:33:30 +0900

    Update DC_main_divide.py

--- DC_main_divide.py
+++ DC_main_divide.py
@@ -22,8 +22,8 @@ def main():
     parser.add_argument('--eval_mode', type=str, default='S', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
     parser.add_argument('--num_exp', type=int, default=1, help='the number of experiments')
     parser.add_argument('--num_eval', type=int, default=1, help='the number of evaluating randomly initialized models')
-    parser.add_argument('--epoch_eval_train', type=int, default=1, help='epochs to train a model with synthetic data')
-    parser.add_argument('--Iteration', type=int, default=1, help='training iterations')
+    parser.add_argument('--epoch_eval_train', type=int, default=150, help='epochs to train a model with synthetic data')
+    parser.add_argument('--Iteration', type=int, default=500, help='training iterations')
     parser.add_argument('--lr_img', type=float, default=0.1, help='learning rate for updating synthetic images')
     parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
     parser.add_argument('--batch_real', type=int, default=32, help='batch size for real data')
@@ -34,7 +34,7 @@ def main():
     parser.add_argument('--save_path', type=str, default='result', help='path to save results')
     parser.add_argument('--dis_metric', type=str, default='ours', help='distance metric')
 ### # 分割数を指定
-    parser.add_argument('--split_num', type=int, default=5, help='number of dataset splits')  
+    parser.add_argument('--split_num', type=int, default=500, help='number of dataset splits')  
 
     args = parser.parse_args()
     args.outer_loop, args.inner_loop = get_loops(args.ipc)
